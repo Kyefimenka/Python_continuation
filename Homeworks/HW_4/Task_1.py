2a06c1036130a4bee3184d13b8ae4083742554a1
Katsiaryna <caelpi85@gmail.com>
2023-10-08 22:51:09 +0200

    add HW_4

--- Homeworks/HW_4/Task_1.py
+++ Homeworks/HW_4/Task_1.py
@@ -1,12 +1,7 @@
 # Напишите функцию для транспонирования матрицы
-from copy import deepcopy
-
 
 def transp_matrix(matrix):
     transposed = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-    # for i in range(len(matrix)):
-    #     for j in range(len(matrix[0])):
-    #         transposed[j][i] = matrix[i][j]
     return transposed
 
 
